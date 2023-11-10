f93db941648b9b6d242439f1bf188baeef4a7573
pocketgodru <126785140+pocketgodru@users.noreply.github.com>
2023-11-10 18:17:27 +0400

    Update segment.py

--- exemple/segment.py
+++ exemple/segment.py
@@ -1,1 +1,12 @@
+from ultralytics import YOLO
+from PIL import Image
+# Load a model
 model = YOLO('model/best_segment.onnx', task='segment')  # загрузка обученной модели для сегментирования 
+
+results = model('img/input_1.jpg') # ваше изображение
+# Show the results
+for r in results:
+    im_array = r.plot()  # plot a BGR numpy array of predictions
+    im = Image.fromarray(im_array[..., ::-1])  # RGB PIL image
+    im.show()  # показ изображения
+    #im.save('results.jpg')  # сохранение изображения
